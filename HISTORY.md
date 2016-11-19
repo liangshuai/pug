a2e51d8063816cdd2a74edd27662dafbce0163b3
liangshuai <liangshuais@qq.com>
2016-11-19 16:52:58 +0800

    init

---
+++ HISTORY.md
@@ -0,0 +1,31 @@
+# Version history
+
+
+## v1.0.3
+* fix: README link
+
+## v1.0.2
+* fix: README link
+
+## v1.0.1
+* chore: Add generated /public directory.
+
+## v1.0.0
+* feat: Release.
+* doc: Improve README.
+
+## v0.3.0
+* feat: Add Gulp task for Pug
+* feat: Add example .pug file
+
+## v0.2.1
+* chore: Configure .gitignore
+
+## v0.2.0
+* chore: Add Pug dependency
+
+## v0.1.1
+* doc: Change README Jade references
+
+## v0.1.0
+* Copied from gulp-less-jade-skeleton
---
+++ gulpfile.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const gulp = require('gulp');
+const less = require('gulp-less');
+const pug = require('gulp-pug');
+const cssmin = require('gulp-cssmin');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const connect = require('gulp-connect');
+
+
+// Source folder configuration
+const SRC_DIR = {};
+SRC_DIR.root = './src/';
+SRC_DIR.assets = SRC_DIR.root + 'assets/';
+SRC_DIR.img = SRC_DIR.root + 'images/';
+SRC_DIR.js = SRC_DIR.root + 'js/';
+SRC_DIR.less = SRC_DIR.root + 'less/';
+SRC_DIR.pug = SRC_DIR.root + 'pug/';
+
+// Source file matchers, using respective directories
+const SRC_FILES = {
+	less: SRC_DIR.less + '*.less',
+	pugTemplates: SRC_DIR.pug + 'templates/*.pug',
+	pug: SRC_DIR.pug + '*.pug',
+	js: SRC_DIR.js + '**/*.js',
+	images: SRC_DIR.img + '**/*',
+	assets: SRC_DIR.assets + '**/*'
+};
+
+// Output directories
+const PUB_DIR = {};
+PUB_DIR.root = './public/';
+PUB_DIR.js = PUB_DIR.root + 'js/';
+PUB_DIR.css = PUB_DIR.root + 'css/';
+PUB_DIR.cssFiles = PUB_DIR.root + 'css/style.css';
+PUB_DIR.fnt = PUB_DIR.root + 'fonts/';
+PUB_DIR.img = PUB_DIR.root + 'images/';
+
+
+// TASKS
+
+gulp.task('watch', () => {
+	gulp.watch(SRC_FILES.less, ['less']);
+	gulp.watch([SRC_FILES.pug,  SRC_FILES.pugTemplates], ['pug']);
+	gulp.watch(SRC_FILES.images, ['imagemin']);
+	gulp.watch(SRC_FILES.assets.onlyCopy, ['copyAssets']);
+});
+
+gulp.task('jsmin', () =>
+	gulp.src(SRC_FILES.js)
+    .pipe(uglify())
+	.pipe(gulp.dest(PUB_DIR.js))
+	.pipe(connect.reload())
+);
+
+gulp.task('less', () =>
+	gulp.src(SRC_FILES.less)
+		.pipe(less().on('error', err => console.log(err)))
+		.pipe(gulp.dest(PUB_DIR.css))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(PUB_DIR.css))
+		.pipe(connect.reload())
+		.pipe(connect.reload())
+);
+
+gulp.task('pug', () =>
+	gulp.src(SRC_FILES.pug)
+		.pipe(pug({
+			pretty: true // Comment this to get minified HTML
+		}))
+		.pipe(gulp.dest(file => {
+			var pugIndex = file.base.lastIndexOf('pug');
+			var relPath = file.base.substr(pugIndex+4);
+			return PUB_DIR.root + relPath;
+		}))
+		.pipe(connect.reload())
+);
+
+gulp.task('imagemin', () =>
+    gulp.src(SRC_FILES.images)
+        .pipe(imagemin())
+        .pipe(gulp.dest(PUB_DIR.img))
+		.pipe(connect.reload())
+);
+
+gulp.task('copyAssets', () =>
+    gulp.src(SRC_FILES.assets)
+        .pipe(gulp.dest(PUB_DIR.root))
+		.pipe(connect.reload())
+);
+
+gulp.task('webserver', () =>
+	connect.server({
+    	root: 'public',
+		livereload: true,
+		port: 8080,
+		host: 'localhost'
+	})
+);
+
+gulp.task('default', ['less', 'pug', 'imagemin', 'jsmin', 'copyAssets']);
+gulp.task('server', ['default', 'webserver', 'watch']);
